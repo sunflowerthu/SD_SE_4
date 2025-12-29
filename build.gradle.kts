6f149c53c8b7a125ba8762952818c9cb7a138e7f
Zarina Ramazanova <maryblacktm@gmail.com>
2025-12-29 12:19:28 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
